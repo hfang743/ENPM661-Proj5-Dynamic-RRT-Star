e8ea0e3dbafbb54c561dea14846524c352953d0c
hfang743 <58123475+hfang743@users.noreply.github.com>
2020-05-06 23:08:01 -0400

    push intersect check

--- RRT_new.py
+++ RRT_new.py
@@ -1,7 +1,6 @@
 import sys, random, math, pygame
 from pygame.locals import *
 from math import sqrt, cos, sin, atan2
-from lineIntersect import *
 import time
 
 # constants
@@ -9,7 +8,7 @@ XDIM = 640
 YDIM = 480
 WINSIZE = [XDIM, YDIM]
 EPSILON = 10.0
-NUMNODES = 8000
+NUMNODES = 5000
 RADIUS = 30.0
 OBS = [(50, 0, 50, 300), (200, 100, 50, 400), (350, 0, 50, 300), (500, 100, 50, 400)]
 
@@ -48,6 +47,34 @@ def drawPath(nodes, pygame, screen):
         pygame.draw.line(screen, red, [last_node.x, last_node.y], [last_node.parent.x, last_node.parent.y], 5)
         last_node = last_node.parent
 
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def checkIntersect(nodeA,nodeB,OBS):
+    A=(nodeA.x,nodeA.y)
+    B=(nodeB.x,nodeB.y)
+    for o in OBS:
+      obs=(o[0],o[1],o[0]+o[2],o[1]+o[3])
+      C1=(obs[0],obs[1])
+      D1=(obs[0],obs[3])
+      C2=(obs[0],obs[1])
+      D2=(obs[2],obs[1])
+      C3=(obs[2],obs[3])
+      D3=(obs[2],obs[1])
+      C4=(obs[2],obs[3])
+      D4=(obs[0],obs[3])
+      inst1= ccw(A,C1,D1) != ccw(B,C1,D1) and ccw(A,B,C1) != ccw(A,B,D1)
+      inst2= ccw(A,C2,D2) != ccw(B,C2,D2) and ccw(A,B,C2) != ccw(A,B,D2)
+      inst3= ccw(A,C3,D3) != ccw(B,C3,D3) and ccw(A,B,C3) != ccw(A,B,D3)
+      inst4= ccw(A,C4,D4) != ccw(B,C4,D4) and ccw(A,B,C4) != ccw(A,B,D4)
+      if inst1==False and inst2==False and inst3==False and inst4==False:
+        #print(A,B)
+        #input("Press Enter to continue...")
+        continue
+      else:
+         return False
+    return True
 
 def checkgoal(newnode, goal):
     if dist([goal.x, goal.y], [newnode.x, newnode.y]) < RADIUS and checkIntersect(newnode, goal, OBS):
